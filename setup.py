f233ca1251b7b26f557b00a017e896b48b6e0ba4
Intiha <gund4422@gmail.com>
2025-10-25 16:18:18 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SimplerJIT",
-    version="0.1.0",
+    version="0.2.0",
     description="Ultra-lightweight Python JIT using TCC and SIMD",
     author="Intiha",
     url="https://github.com/Gund4422/SimplerJIT",
@@ -11,10 +11,7 @@ setup(
     install_requires=[
         "numpy>=1.21"
     ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: WTFPL",
-    ],
     include_package_data=True,
 )
+
 
